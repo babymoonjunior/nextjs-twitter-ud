32fd7b499504a9c883df013ecce76f722292ee2c
PreeyanuchYu <babymoonshop@gmail.com>
2023-10-27 03:15:44 +0700

    Refactor: db.ts connection

--- db.ts
+++ db.ts
@@ -4,13 +4,13 @@ import { loadEnvConfig } from "@next/env";
 const projectDir = process.cwd();
 loadEnvConfig(projectDir);
 
-export async function getClient(){
-    const client = new Client({
-        user: process.env.POSTGRES_USER,
-        host: process.env.POSTGRES_HOST,
-        database: process.env.POSTGRES_DATABASE,
-        password: process.env.POSTGRES_PASSWORD,
-        port: parseInt(process.env.POSTGRES_PORT!),
-      });
-      return client;
+export async function getClient() {
+  const client = new Client({
+    user: process.env.POSTGRES_USER,
+    host: process.env.POSTGRES_HOST,
+    database: process.env.POSTGRES_DATABASE,
+    password: process.env.POSTGRES_PASSWORD,
+    port: parseInt(process.env.POSTGRES_PORT!),
+  });
+  return client;
 }
