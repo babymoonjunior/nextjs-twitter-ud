2af4df1272258c02c758c8fc8a86cf3f18626772
PreeyanuchYu <babymoonshop@gmail.com>
2023-10-27 02:45:00 +0700

    Add bcrypt

--- scripts/load-fake-data.ts
+++ scripts/load-fake-data.ts
@@ -1,6 +1,7 @@
 import { Client } from "pg";
 import { loadEnvConfig } from "@next/env";
 import { faker } from "@faker-js/faker";
+import bcrypt from "bcrypt";
 
 const projectDir = process.cwd();
 loadEnvConfig(projectDir);
@@ -22,9 +23,12 @@ async function loadFakeData(numUsers: number = 10) {
 
     //load fake USERS
     for (let i = 0; i < numUsers; i++) {
+      const saltRounds = 10;
+      const hash = await bcrypt.hash("twitter123", saltRounds);
+
       await client.query(
         "insert into public.users (username, password, avatar) values ($1, $2, $3)",
-        [faker.internet.userName(), "password", faker.image.avatar()]
+        [faker.internet.userName(), hash, faker.image.avatar()]
       );
     }
 
