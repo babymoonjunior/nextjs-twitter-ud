682601eb3e1f935911b3584a44ebe0a54db94df6
PreeyanuchYu <babymoonshop@gmail.com>
2023-10-29 22:48:35 +0700

    Feat: add profile endpoint & jwt verification

---
+++ app/api/users/profile/route.ts
@@ -0,0 +1,18 @@
+import { getJWTPayload } from "@/app/utils/auth";
+import { sql } from "@/db";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  // get currently logged in user
+  const jwtPayload = await getJWTPayload();
+
+  //fetch user data
+  const res = await sql(
+    "select id, username, avatar from users where id = $1",
+    [jwtPayload.sub]
+  ); //.sub คือ user id
+  const user = res.rows[0];
+
+  //return user data
+  return NextResponse.json({ data: user})
+}
