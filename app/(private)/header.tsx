391e7bbe9bd30a42d11dd8e604fbd24e6bc342c2
PreeyanuchYu <babymoonshop@gmail.com>
2023-10-30 14:15:59 +0700

    Feat: install swr & fetcher in Header

--- app/(private)/header.tsx
+++ app/(private)/header.tsx
@@ -1,5 +1,28 @@
+"use client";
+
+import useSWR from "swr";
+
+// fetcher จะใข้ axios ก็ได้
+const fetcher = async (url: RequestInfo | URL) => {
+    const res = await fetch(url);
+    if(!res.ok){
+        const msg = "An error occurred while fetching the data.";
+        const info = await res.json();
+        const status = res.status;
+        const error = new Error(msg);
+        console.error(info, status);
+        throw error;
+    }
+    return res.json();
+}
+
 export default function Header(){
+    const { data, error, isLoading } = useSWR("/api/users/profile", fetcher);
+
+    if (error) return <div>failed to load</div>
+    if (isLoading) return <div>loading...</div>
+    console.log(data);
     return(
-        <header>Header</header>
+        <header>{data.data.username}</header>
     )
 }
