fc26df01850fe59b71055c881fbbd998a906e950
PreeyanuchYu <babymoonshop@gmail.com>
2023-10-30 15:25:11 +0700

    Feat: add User component in header

--- app/(private)/header.tsx
+++ app/(private)/header.tsx
@@ -1,13 +1,21 @@
 "use client";
 import useSWR from "swr";
+import User from "../components/User";
 
 export default function Header(){
     const { data, error, isLoading } = useSWR("/api/users/profile");
 
     if (error) return <div>failed to load</div>
     if (isLoading) return <div>loading...</div>
-    console.log(data);
+    
     return(
-        <header>{data.data.username}</header>
+        <header className="text-white flex flex-row w-full p-5 bg-slate-800 rounded-lg my-2 justify-between items-center">
+            <div>
+                <h1 className="font-mono text-lg">Strings</h1>
+            </div>
+            <div>
+                <User user={data.data} href="account"/>
+            </div>
+        </header>
     )
 }
---
+++ app/types.ts
@@ -0,0 +1,6 @@
+interface UserI {
+    id: number;
+    username: string;
+    avatar: string;
+}
+
