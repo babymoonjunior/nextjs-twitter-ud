778048e257951db94dde87996a8611520b1ae3be
PreeyanuchYu <babymoonshop@gmail.com>
2023-11-07 00:48:10 +0700

    Feat: account page & logout api

---
+++ app/(private)/account/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import AvatarForm from "./AvatarForm";
+import SignOutButton from "./SignOutBTN";
+
+export default function AccountPage(){
+    return(
+        <div>
+            <h2>Account</h2>
+            <AvatarForm />
+            <SignOutButton />
+        </div>
+    )
+}
