956f87e2f574e9a403ecb4195e05dd60e81159c2
PreeyanuchYu <babymoonshop@gmail.com>
2023-11-06 22:49:25 +0700

    Feat: following page UI & API

---
+++ app/(private)/following/FollowingList.tsx
@@ -0,0 +1,21 @@
+import User from "@/app/components/User";
+import useSWR from "swr";
+
+export default function FollowingList( {index} : {index: number}){
+    const { data: userData } = useSWR("/api/users/profile");
+    const { data: followerData} = useSWR(() => "/api/users/" + userData.data.id + "/following?page=" + index)
+
+    if(!followerData) return <div>loading...</div>
+
+    return(
+        <ul>
+            {followerData.data.map((user:UserI) => {
+                return(
+                    <li className="my-5" key={user.id}>
+                        <User user={user} />
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
